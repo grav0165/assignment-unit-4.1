f95b86a7b43fddb555a7277d84cb6dc1e8ddf64c
Aubrey Graves <grav0165@gmail.com>
2023-05-25 10:12:55 -0500

    reviewed Q2

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,7 +15,7 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return "Hello, " + name + "!"
+  return "Hello, " + name + "!";
 }
 // Remember to call the function to test
 console.log(helloName('Aubrey'));
